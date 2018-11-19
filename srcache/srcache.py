d5aa423f9c3543483eaefd644544eb82b3c4b923
lloydzhou <lloydzhou@qq.com>
2018-11-19 10:45:47 +0800

    增加支持async

--- srcache/srcache.py
+++ srcache/srcache.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import redis
-
-from tornado.options import options
-import pickle
+import random
 import functools
 import logging
-import random
+import pickle
+from inspect import iscoroutinefunction
+
+import redis
 from binascii import crc32
 from tornado.ioloop import IOLoop
+from tornado.options import options
 
 
 redispool = redis.ConnectionPool(
@@ -62,7 +63,41 @@ def stalecache(key=None, prefix=None, attr_key=None, attr_prefix=None,
                 IOLoop.current().add_timeout(IOLoop.current().time() + real_time_delay, func)
 
             return v
-        return wrapper
+
+        @functools.wraps(method)
+        async def async_wrapper(self, *args, **kwargs):
+            if kwargs.get('skip_cache'):
+                return await method(self, *args, **kwargs)
+
+            name = key or kwargs.get('key', None) or (attr_key and getattr(self, attr_key))
+            if not name:
+                _prefix = prefix or (attr_prefix and getattr(self, attr_prefix)) or gen_prefix(self, method)
+                name = "%s:%u" % (_prefix, crc32(pickle.dumps(args) + pickle.dumps(kwargs)))
+            res = client().pipeline().ttl(name).get(name).execute()
+            v = pickle.loads(res[1]) if res[0] > 0 and res[1] else None
+            if res[0] <= 0 or res[0] < stale:
+
+                async def func():
+                    value = await method(self, *args, **kwargs)
+                    logging.debug("update cache: %s", name)
+                    client().pipeline().set(
+                        name, pickle.dumps(value)
+                    ).expire(name, expire + stale).execute()
+                    return value
+
+                # create new cache in blocking modal, if cache not exists.
+                if res[0] <= 0:
+                    return await func()
+
+                # create new cache in non blocking modal, and return stale data.
+                # set expire to get a "lock", and delay to run the task
+                real_time_delay = random.randrange(time_delay, max_time_delay)
+                client().expire(name, stale + real_time_delay + time_lock)
+                IOLoop.current().add_timeout(IOLoop.current().time() + real_time_delay, func)
+
+            return v
+
+        return async_wrapper if iscoroutinefunction(method) else wrapper
     return _
 
 
